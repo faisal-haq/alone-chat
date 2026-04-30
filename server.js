7ece80acc8dd42cd15e1c1f3a2d986cf17101761
faisal-haq <faisalhaq73@gmail.com>
2026-04-30 23:16:29 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -180,7 +180,7 @@ socket.on('disconnect', () => {
   });
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 const HOST = process.env.HOST || '0.0.0.0';
 
 server.listen(PORT, HOST, () => {
