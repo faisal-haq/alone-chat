8a66fa1f5f0126316d26c76918d36b70d9aba4f1
faisal-haq <faisalhaq73@gmail.com>
2026-05-05 01:07:41 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -12,28 +12,22 @@ app.use(express.static(path.join(__dirname)));
 let waitingVideoUsers = [];
 let waitingTextUsers = [];
 
-// ✅ REMOVE SOCKET FROM WAITING LIST
 function removeFromWaiting(socket) {
   waitingVideoUsers = waitingVideoUsers.filter(s => s.id !== socket.id);
   waitingTextUsers = waitingTextUsers.filter(s => s.id !== socket.id);
 }
 
-// ✅ MATCH USERS
 function findMatch(socket, mode) {
   const list = mode === 'video' ? waitingVideoUsers : waitingTextUsers;
-
-  // Remove self duplicates first
   removeFromWaiting(socket);
 
   if (list.length > 0) {
     const partner = list.shift();
-
     socket.partner = partner;
     partner.partner = socket;
 
     socket.emit('matched', { partnerId: partner.id });
     partner.emit('matched', { partnerId: socket.id });
-
   } else {
     list.push(socket);
     socket.emit('waiting');
@@ -41,8 +35,6 @@ function findMatch(socket, mode) {
 }
 
 io.on('connection', (socket) => {
-  console.log("Connected:", socket.id);
-
   socket.on('join', (data = {}) => {
     socket.mode = data.mode || 'video';
     findMatch(socket, socket.mode);
@@ -50,47 +42,29 @@ io.on('connection', (socket) => {
 
   socket.on('offer', (data) => {
     if (socket.partner) {
-      socket.partner.emit('offer', {
-        sdp: data.sdp,
-        senderId: socket.id
-      });
+      socket.partner.emit('offer', { sdp: data.sdp, senderId: socket.id });
     }
   });
 
   socket.on('answer', (data) => {
     if (socket.partner) {
-      socket.partner.emit('answer', {
-        sdp: data.sdp,
-        senderId: socket.id
-      });
+      socket.partner.emit('answer', { sdp: data.sdp, senderId: socket.id });
     }
   });
 
   socket.on('ice-candidate', (data) => {
     if (socket.partner) {
-      socket.partner.emit('ice-candidate', {
-        candidate: data.candidate,
-        senderId: socket.id
-      });
+      socket.partner.emit('ice-candidate', { candidate: data.candidate });
     }
   });
 
-  socket.on('message', (data) => {
-    if (socket.partner) {
-      socket.partner.emit('message', data);
-    }
-  });
-
-  // ✅ FIXED NEXT
   socket.on('next', () => {
     removeFromWaiting(socket);
 
     if (socket.partner) {
       const partner = socket.partner;
-
       socket.partner = null;
       partner.partner = null;
-
       partner.emit('partner-left');
     }
 
@@ -98,8 +72,6 @@ io.on('connection', (socket) => {
   });
 
   socket.on('disconnect', () => {
-    console.log("Disconnected:", socket.id);
-
     removeFromWaiting(socket);
 
     if (socket.partner) {
@@ -109,7 +81,6 @@ io.on('connection', (socket) => {
   });
 });
 
-server.listen(3001, () => {
-  console.log("Server running on port 3001");
-});      
-      
+server.listen(3000, () => {
+  console.log("Server running on http://localhost:3000");
+});
